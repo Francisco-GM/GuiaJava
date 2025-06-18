9ed1ac52118e842918f745956e5d89c12da42a95
Francisco-GM <Francisco.Gonzlez.7MY@gmail.com>
2025-06-17 22:43:56 -0600

    HolaMundo

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+   // Funcion main (funcion principal)
+   public static void main(String[] args) { // Comando atajo psvm
+       System.out.println("Hola Mundo con Java"); //comando atajo sout (mandar informacion a la consola)
+       System.out.println("Saludos!!");
+   } // Fin del metodo Main
+}
