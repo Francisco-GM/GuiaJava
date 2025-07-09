ee1039b34750ae215eced10b7cac9c48fb3ed2af
Francisco-GM <Francisco.Gonzlez.7MY@gmail.com>
2025-07-08 22:32:33 -0600

    Ejercicio01

---
+++ IntroduccionJava/src/Ejercicio01.java
@@ -0,0 +1,9 @@
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es Francisco Monrroy");
+
+        System.out.println("Tengo 27 years old");
+
+        System.out.println("Soy de Mexico");
+    }
+}
